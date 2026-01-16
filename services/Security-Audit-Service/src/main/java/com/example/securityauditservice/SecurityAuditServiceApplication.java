61da3caa382e14144ab75b925a6e3782725b331e
Mohamed <enahari2000@gmail.com>
2026-01-16 02:27:15 +0100

    add: pom dependencies

---
+++ services/Security-Audit-Service/src/main/java/com/example/securityauditservice/SecurityAuditServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityauditservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAuditServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAuditServiceApplication.class, args);
+    }
+
+}
---
+++ services/Security-Audit-Service/src/test/java/com/example/securityauditservice/SecurityAuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityauditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAuditServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
